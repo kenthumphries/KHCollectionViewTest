8f74b31928f7b19ee13d7fa14e60eba45c90d4bc
Kent Humphries <kent.humphries@gmail.com>
2016-08-11 23:27:51 +1000

    Add guard to clean up code.

--- CollectionViewTest/SimpleDataSource.swift
+++ CollectionViewTest/SimpleDataSource.swift
@@ -20,25 +20,26 @@ class SimpleDataSource: NSObject, UICollectionViewDataSource {
     }
     
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("CellReuseIdentifier", forIndexPath: indexPath)
+        let newCell = collectionView.dequeueReusableCellWithReuseIdentifier("CellReuseIdentifier", forIndexPath: indexPath)
         
-        if let cell = cell as? Cell {
-            
-            cell.title.text = "\(indexPath.section).\(indexPath.item)"
-            cell.title.sizeToFit()
-            
-            if (collectionView.tag == 0) {
-                cell.contentView.backgroundColor = UIColor.random(red: 1.00) // Make random self color, but red-ish
-            } else {
-                cell.contentView.backgroundColor = UIColor.random(green: 1.00) // Make random self color, but green-ish
-            }
-            
-            let totalItems = collectionView.numberOfItemsInSection(indexPath.section)
-            // Make cells fading in opacity down to 0.25 for last item
-            // First quarter of items will be > 1.0 which is interpreted as 1.00
-            let alpha = 1.25 - (CGFloat(indexPath.item) / CGFloat(totalItems))
-            cell.contentView.backgroundColor = cell.contentView.backgroundColor?.colorWithAlphaComponent(alpha)
+        guard let cell = newCell as? Cell else {
+            return newCell
         }
+        
+        cell.title.text = "\(indexPath.section).\(indexPath.item)"
+        cell.title.sizeToFit()
+        
+        if (collectionView.tag == 0) {
+            cell.contentView.backgroundColor = UIColor.random(red: 1.00) // Make random self color, but red-ish
+        } else {
+            cell.contentView.backgroundColor = UIColor.random(green: 1.00) // Make random self color, but green-ish
+        }
+        
+        let totalItems = collectionView.numberOfItemsInSection(indexPath.section)
+        // Make cells fading in opacity down to 0.25 for last item
+        // First quarter of items will be > 1.0 which is interpreted as 1.00
+        let alpha = 1.25 - (CGFloat(indexPath.item) / CGFloat(totalItems))
+        cell.contentView.backgroundColor = cell.contentView.backgroundColor?.colorWithAlphaComponent(alpha)
         
         return cell
     }
