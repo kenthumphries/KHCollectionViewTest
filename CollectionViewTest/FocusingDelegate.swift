5a6a501d1898c8e5f7930fcc25945b4da61b2a6d
Kent Humphries <kent.humphries@gmail.com>
2016-08-15 19:11:51 +1000

    Create FocusingDelegate that implements only focusing functionality for a UICollectionViewDelegate. Update SimpleDelegate to subclass FocusingDelegate.

---
+++ CollectionViewTest/FocusingDelegate.swift
@@ -0,0 +1,33 @@
+//
+//  FocusingDelegate.swift
+//  CollectionViewTest
+//
+//  Created by Kent Humphries on 15/08/2016.
+//  Copyright © 2016 KentHumphries. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class FocusingDelegate: NSObject, UICollectionViewDelegateFlowLayoutFocusing // Ensure that the CollectionView is Focusing
+{
+    var focusedIndexPath = NSIndexPath(forItem: 0, inSection: 0)
+}
+
+// MARK: Call UICollectionViewDelegateFlowLayoutFocusing methods on scrolling or external contentOffset change
+extension FocusingDelegate {
+    
+    func scrollViewDidEndDragging(scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        if !decelerate {
+            self.collectionViewDidEndScrolling(scrollView)
+        }
+    }
+    
+    func scrollViewDidEndDecelerating(scrollView: UIScrollView) {
+        self.collectionViewDidEndScrolling(scrollView)
+    }
+    
+    func collectionView(collectionView: UICollectionView, targetContentOffsetForProposedContentOffset proposedContentOffset: CGPoint) -> CGPoint {
+        return self.focussedContentOffset(collectionView, proposedContentOffset: proposedContentOffset)
+    }
+}
--- CollectionViewTest/SimpleDelegate.swift
+++ CollectionViewTest/SimpleDelegate.swift
@@ -9,14 +9,12 @@
 import Foundation
 import UIKit
 
-class SimpleDelegate: NSObject, UICollectionViewDelegateFlowLayoutFocusing // Ensure that the CollectionView is Focusing
+class SimpleDelegate: FocusingDelegate // Ensure that the CollectionView is Focusing
 {
     
     var didSelectBlock : (collectionViewFlowLayout: UICollectionViewFlowLayout, indexPath: NSIndexPath) -> () = { (layout, indexPath) in
         // Do nothing by default
     }
-    
-    var focusedIndexPath = NSIndexPath(forItem: 0, inSection: 0)
 }
 
 extension SimpleDelegate {
@@ -27,26 +25,5 @@ extension SimpleDelegate {
         }
         
         self.didSelectBlock(collectionViewFlowLayout: currentLayout, indexPath: indexPath)
-    }
-}
-
-// MARK: Call UICollectionViewDelegateFlowLayoutFocusing methods on scrolling or external contentOffset change
-extension SimpleDelegate {
-    /* This should ideally be an extension of UICollectionViewDelegate where Self:UICollectionViewDelegateFlowLayoutFocusing.
-       This could then be stored in UICollectionViewDelegateFlowLayoutFocusing and defined once for ALL UICollectionViewDelegateFlowLayoutFocusing objects.
-       Alas, this does not work in Swift 2.2 (http://stackoverflow.com/questions/32542362/swift-2-0-uitextfielddelegate-protocol-extension-not-working) */
-    
-    func scrollViewDidEndDragging(scrollView: UIScrollView, willDecelerate decelerate: Bool) {
-        if !decelerate {
-            self.collectionViewDidEndScrolling(scrollView)
-        }
-    }
-    
-    func scrollViewDidEndDecelerating(scrollView: UIScrollView) {
-        self.collectionViewDidEndScrolling(scrollView)
-    }
-    
-    func collectionView(collectionView: UICollectionView, targetContentOffsetForProposedContentOffset proposedContentOffset: CGPoint) -> CGPoint {
-        return self.focussedContentOffset(collectionView, proposedContentOffset: proposedContentOffset)
     }
 }
